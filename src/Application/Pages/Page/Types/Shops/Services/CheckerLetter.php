5c41bddb070dff27df79828e33dd8e171e8f0f18
Michał Materniak <michal.materniak@commit-m.pl>
2020-11-13 06:03:27 +0100

    404 na nieistniejącą stronę paginacji w sklepach

---
+++ src/Application/Pages/Page/Types/Shops/Services/CheckerLetter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Application\Pages\Page\Types\Shops\Services;
+
+use App\Services\System\Request\Retrievers\RequestDataManager;
+
+class CheckerLetter
+{
+    /**
+     * @var mixed|null $letter
+     */
+    protected $letter;
+
+    /**
+     * @var RequestDataManager $requestDataManager
+     */
+    protected $requestDataManager;
+
+    public function __construct(RequestDataManager $requestDataManager)
+    {
+        $this->requestDataManager = $requestDataManager;
+        $this->letter = $requestDataManager->getValue('letter', null);
+    }
+
+    public function checkLetter()
+    {
+        return $this->letterExist()
+            && strlen($this->letter) === 1
+            && preg_match_all('/^([a-z]|[A-Z]|[0-9])$/', $this->letter, $matches);
+    }
+
+    public function letterExist()
+    {
+        return $this->requestDataManager->checkIfExist('letter');
+    }
+}
--- src/Controller/Front/Api/Shops/ShopsController.php
+++ src/Controller/Front/Api/Shops/ShopsController.php
@@ -2,29 +2,45 @@
 namespace App\Controller\Front\Api\Shops;
 
 use App\Application\Pages\Page\Page;
+use App\Application\Pages\Page\Types\Shops\Services\CheckerLetter;
 use App\Application\Pages\Page\Types\Shops\Shops;
 use App\Controller\Front\Api\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 
 class ShopsController extends AbstractController
 {
     /**
      * @param Shops $page
+     * @param CheckerLetter $checerkLetter
      * @return JsonResponse
      * @throws ExceptionInterface
      */
-    public function shops(Shops $page)
+    public function shops(Shops $page, CheckerLetter $checerkLetter)
     {
-        $this->templateVars->insert('page', $this->normalizer->normalize($page, null, [
-            'groups' => ['resource', 'seo', 'subpage'],
-        ]));
+        if ($checerkLetter->letterExist() && !$checerkLetter->checkLetter()) {
+            throw new NotFoundHttpException();
+        } else {
+            $this->templateVars->insert('page', $this->normalizer->normalize($page, null, [
+                'groups' => ['resource', 'seo', 'subpage'],
+            ]));
 
-        return $this->responseJson();
+            return $this->responseJson();
+        }
     }
 
-    public function index(Page $page)
+    /**
+     * @param Page $page
+     * @param CheckerLetter $checerkLetter
+     * @return JsonResponse
+     * @throws ExceptionInterface
+     */
+    public function index(Page $page, CheckerLetter $checerkLetter)
     {
+        if ($checerkLetter->letterExist() && !$checerkLetter->checkLetter()) {
+            throw new NotFoundHttpException();
+        }
         $this->templateVars->insert('model', $this->normalizer->normalize($page, null, [
             'groups' => ['resource', 'seo', 'subpage'],
         ]));
