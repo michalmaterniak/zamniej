266cd21f4562dc0bae48b38f78ef2e1eed2d482f
Michał Materniak <michal.materniak@commit-m.pl>
2020-11-15 13:20:36 +0100

    Naprawa sitemapy - link do api

--- assets/front/config/sitemap.js
+++ assets/front/config/sitemap.js
@@ -1,14 +1,16 @@
 const axios = require('axios')
-let baseURL = process.env.NODE_ENV === 'production' ? process.env.PROD_HOST : process.env.DEV_HOST;
+let baseURLAPI = process.env.NODE_ENV === 'production' ? process.env.PROD_HOST : process.env.DEV_HOST;
+let hostName = process.env.NODE_ENV === 'production' ? process.env.BASE_URL : process.env.DEV_HOST;
 export default {
   path: '/sitemap.xml',
-    routes: async () => {
-      const {data} = await axios.post(`http://apizamniej.loc/page/api/sitemap`)
-      return data.urls.map((url) => url.path)
-    },
-    defaults: {
-      changefreq: 'daily',
-      priority: 1,
-      lastmod: new Date()
-    }
+  hostname: hostName,
+  routes: async () => {
+    const {data} = await axios.post(`${baseURLAPI}page/api/sitemap`)
+    return data.urls.map((url) => url.path)
+  },
+  defaults: {
+    changefreq: 'daily',
+    priority: 1,
+    lastmod: new Date()
+  }
 }
