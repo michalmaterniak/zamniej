a7022adb27cc2dadec8c0a98e23bf7ea424d6ff4
Michał Materniak <michal.materniak@commit-m.pl>
2021-01-10 13:20:20 +0000

    Resolve "Zmienić powiadomienia o nowych programach"

--- assets/admin/src/components/Menu/MenuElements/Messages.vue
+++ assets/admin/src/components/Menu/MenuElements/Messages.vue
@@ -7,50 +7,19 @@
       <ul class="list-style-none">
         <li>
           <div class="">
-            <!-- Message -->
+            <!-- Message webepartners -->
             <a href="javascript:void(0)" class="link border-top" v-if="newProgramsCount > 0">
               <div class="d-flex no-block align-items-center p-10">
                 <span class="btn btn-success btn-circle"><i class="ti-calendar"></i></span>
                 <div class="m-l-10">
-                  <h5 class="m-b-0">Nowe progrmy (<span v-text="newProgramsCount"></span>)</h5>
+                  <h5 class="m-b-0">Nowe progrmy webepartners (<span v-text="newProgramsCount"></span>)</h5>
                   <span class="mail-desc">
                     <router-link :to="{name: 'panel-affiliations-webepartners-programs-new'}" >Zaakceptuj program afiliacyjny</router-link>
                   </span>
                 </div>
               </div>
             </a>
-
-
-<!--            &lt;!&ndash; Message &ndash;&gt;-->
-<!--            <a href="javascript:void(0)" class="link border-top">-->
-<!--              <div class="d-flex no-block align-items-center p-10">-->
-<!--                <span class="btn btn-info btn-circle"><i class="ti-settings"></i></span>-->
-<!--                <div class="m-l-10">-->
-<!--                  <h5 class="m-b-0">Settings</h5>-->
-<!--                  <span class="mail-desc">You can customize this template</span>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </a>-->
-<!--            &lt;!&ndash; Message &ndash;&gt;-->
-<!--            <a href="javascript:void(0)" class="link border-top">-->
-<!--              <div class="d-flex no-block align-items-center p-10">-->
-<!--                <span class="btn btn-primary btn-circle"><i class="ti-user"></i></span>-->
-<!--                <div class="m-l-10">-->
-<!--                  <h5 class="m-b-0">Pavan kumar</h5>-->
-<!--                  <span class="mail-desc">Just see the my admin!</span>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </a>-->
-<!--            &lt;!&ndash; Message &ndash;&gt;-->
-<!--            <a href="javascript:void(0)" class="link border-top">-->
-<!--              <div class="d-flex no-block align-items-center p-10">-->
-<!--                <span class="btn btn-danger btn-circle"><i class="fa fa-link"></i></span>-->
-<!--                <div class="m-l-10">-->
-<!--                  <h5 class="m-b-0">Luanch Admin</h5>-->
-<!--                  <span class="mail-desc">Just see the my new admin!</span>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </a>-->
+            <!-- Message webepartners -->
           </div>
         </li>
       </ul>
