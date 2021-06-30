e9ad15bcce4e9ab43d5d36c3a940ca1531940e7d
Michał Materniak <michal.materniak@commit-m.pl>
2021-06-30 23:48:27 +0200

    po kliknięciu wyłączono automatyczne przejście

--- assets/front/components/Popup/Components/CodeClipboard.vue
+++ assets/front/components/Popup/Components/CodeClipboard.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <span v-if="isTouchDevice && isClickCoupon" class="green" v-text="'Skopiowano!'"></span>
-    <a @mouseenter="setTextButton(code)" @mouseleave="setTextButton(null)" href="#" class="btn btn-solid " id="clipboard-code" :class="{clicked: isClickCoupon}" v-clipboard:copy="code" v-clipboard:success="clipboard">
-<!--      {{ code }}-->
-      <span v-if="!isClickCoupon"><span v-text="showingTextButton" ></span></span><span v-else>Skopiowano</span></a>
+    <div v-if="isTouchDevice && isClickCoupon" class="green" v-text="'Skopiowano!'"></div>
+    <div>
+      <a @mouseenter="setTextButton(code)" @mouseleave="setTextButton(null)" href="#" class="btn btn-solid const-width" id="clipboard-code" :class="{clicked: isClickCoupon}" v-clipboard:copy="code" v-clipboard:success="clipboard">
+        <!--      {{ code }}-->
+        <span v-if="!isClickCoupon"><span v-text="showingTextButton" ></span></span><span v-else>Skopiowano</span></a>
+    </div>
   </div>
-
 </template>
 <script>
 export default {
@@ -42,12 +43,12 @@ export default {
 </script>
 
 <style scoped>
-.itg-btn
-{
-  height: 50px;
+.const-width {
+  width: 320px;
 }
-.itg-btn.box-btn.clicked span
-{
-  left: -95%
+@media (max-width: 576px) {
+  .const-width {
+    width: 220px;
+  }
 }
 </style>
